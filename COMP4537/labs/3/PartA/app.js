a2201d1666592cbab6173f15fa0c02f439fb80af
Alex Choi <sequoia.alex.ca@gmail.com>
2025-09-24 11:11:35 -0700

    first commit

---
+++ COMP4537/labs/3/PartA/app.js
@@ -0,0 +1,4 @@
+const module = require('./module/math');
+
+console.log(`Hello Alex Choi. The sum of 2 and 5 is ${module.add(2, 5)}`); // Output: 7
+console.log(`Hello Alex Choi. The difference of 5 and 2 is ${module.subtract(5, 2)}`); // Output: 3
---
+++ COMP4537/labs/3/PartA/module/math.js
@@ -0,0 +1,19 @@
+/**
+ * Adds two numbers.
+ * @param {*} x | The first number
+ * @param {*} y | The second number
+ * @returns | The sum of the two numbers
+ */
+function add(x, y) {
+    return x + y;
+}
+
+/**
+ * Subtracts two numbers.
+ * @param {*} x | The first number
+ * @param {*} y | The second number
+ * @returns | The difference of the two numbers
+ */
+function subtract(x, y) {
+    return x - y;
+}
