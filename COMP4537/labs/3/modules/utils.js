efb1e495172ebd5362ff0e88d33490724868d575
Alex Choi <sequoia.alex.ca@gmail.com>
2025-09-28 16:44:51 -0700

    Moved all logic to class util

--- COMP4537/labs/3/modules/utils.js
+++ COMP4537/labs/3/modules/utils.js
@@ -1,4 +1,8 @@
 import { LangEn } from "../lang/en/en.js";
+import fs from 'fs';
+
+const UTF8 = 'utf8';
+const NEW_LINE = '\n';
 
 /**
  * Utility class providing helper methods for greeting and file content display.
@@ -35,5 +39,81 @@ export class Utils {
      */
     readFileContent(fileName, data) {
         return `${this.lang.FILE_CONTENT}${fileName}${this.lang.FILE_CONTENT2}${data}${this.lang.CLOSE_PRE}${this.lang.HOME_BUTTON}`;
+    }
+
+    /**
+	 * Handles reading a file and sending the response.
+	 * @param {*} fileName | Name of the file to read
+	 * @param {*} res | Response object
+	 * @param {*} statusCodes | Status codes object
+	 * @param {*} contentTypes | Content types object
+	 */
+    handleReadFile(fileName, res, statusCodes, contentTypes) {
+        const { NOT_FOUND_STATUS, SUCCESS_STATUS } = statusCodes;
+        const { TEXT_HTML } = contentTypes;
+
+        fs.readFile(fileName, UTF8, (err, data) => {
+            if (err) {
+                res.writeHead(NOT_FOUND_STATUS, { CONTENT_TYPE: TEXT_HTML });
+                res.end(`${this.lang.ERROR}${this.lang.FILE_READ_ERROR}${this.lang.TEXT_FILE}${fileName}${this.lang.HYPHEN}${err.message}${this.lang.CLOSE_P}${this.lang.AUTO_HOME_RETURN}`);
+                return;
+            }
+            res.writeHead(SUCCESS_STATUS, { CONTENT_TYPE: TEXT_HTML });
+            res.end(this.readFileContent(fileName, data));
+        });
+    }
+
+    /**
+	 * Handles writing to a file and sending the response.
+	 * @param {*} fileName | Name of the file
+	 * @param {*} dataToAppend | Data to append to the file
+	 * @param {*} res | Response object
+	 * @param {*} statusCodes | Status codes object
+	 * @param {*} contentTypes | Content types object
+	 * @param {*} successMsg | Success message
+	 * @param {*} errorMsg | Error message
+	 * @param {*} autoReturnHtml | HTML for auto return
+	 */
+    handleWriteFile(fileName, dataToAppend, res, statusCodes, contentTypes, successMsg, errorMsg, autoReturnHtml) {
+        const { SUCCESS_STATUS, NOT_FOUND_STATUS } = statusCodes;
+        const { TEXT_HTML } = contentTypes;
+
+        fs.appendFile(fileName, dataToAppend, { encoding: UTF8 }, (err) => {
+            let pageMessage = '';
+            if (err) {
+                console.error(errorMsg, err);
+                pageMessage = `${this.lang.ERROR}${errorMsg} ${err.message}${this.lang.CLOSE_P}`;
+            } else {
+                pageMessage = `${this.lang.SUCCESS}${successMsg}${this.lang.TEXT_ADDED}${dataToAppend.trim()}${this.lang.TEXT_TO}${fileName}${this.lang.CLOSE_P}`;
+            }
+            res.writeHead(SUCCESS_STATUS, { CONTENT_TYPE: TEXT_HTML });
+            res.end(pageMessage + autoReturnHtml);
+        });
+    }
+
+    /**
+	 * Handles greeting logic and file logging.
+	 * @param {*} name | Name of the person to greet
+	 * @param {*} now | Current date
+	 * @param {*} res | Response object
+	 * @param {*} statusCodes | Status codes object
+	 * @param {*} contentTypes | Content types
+	 * @param {*} fileName | File to log the greeting message
+	 */
+    handleGreeting(name, now, res, statusCodes, contentTypes, fileName) {
+        const { SUCCESS_STATUS } = statusCodes;
+        const { TEXT_HTML } = contentTypes;
+        const greetingMessage = this.greeting(name, now);
+        let html = this.getDate(greetingMessage);
+        const logMessage = `${greetingMessage.trim()}${NEW_LINE}`;
+        fs.appendFile(fileName, logMessage + NEW_LINE, { encoding: UTF8 }, (err) => {
+            if (err) {
+                console.error(this.lang.MESSAGE.FILE_WRITE_ERROR, err);
+            } else {
+                console.log(this.lang.MESSAGE.FILE_WRITE_SUCCESS);
+            }
+        });
+        res.writeHead(SUCCESS_STATUS, { CONTENT_TYPE: TEXT_HTML });
+        res.end(html);
     }
 }
--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,14 +1,10 @@
 /**
  * Alex Choi - A01323994
- * Lab 3 - Basic HTTP Server with File Operations
- * COMP4537 - Web Development
- * Date: September 2025
  * I acknowledge that I used VSCode Copilot to assist in the development of this code.
  */
 
 import { createServer } from 'http';
 import { parse } from 'url';
-import fs from 'fs';
 import { Utils } from './modules/utils.js';
 import { LangEn } from './lang/en/en.js';
 
@@ -34,9 +30,8 @@ const FILES = {
 	WRITING_FILE: 'file.txt'
 };
 const REGEX_TEXT = `^${ROUTES.READ_FILE}/([^/]+\.txt)$`;
-const UTF8 = 'utf8';
 const NEW_LINE = '\n';
-const CONTENT_TYPE = 'Content-Type';
+const FIRST_CAPTURING_GROUP = 1;
 
 /**
  * LabServer class to handle HTTP requests and responses.
@@ -69,16 +64,8 @@ class LabServer {
 		if (req.method === HTTP_METHODS.GET) {
 			const readFileMatch = pathname.match(this.READ_FILE_PATTERN);
 			if (readFileMatch) {
-				const fileName = readFileMatch[1];
-				fs.readFile(fileName, UTF8, (err, data) => {
-					if (err) {
-						res.writeHead(NOT_FOUND_STATUS, { CONTENT_TYPE: CONTENT_TYPES.TEXT_HTML });
-						res.end(`${this.lang.ERROR}${this.lang.FILE_READ_ERROR}${this.lang.TEXT_FILE}${fileName}${this.lang.HYPHEN}${err.message}${this.lang.CLOSE_P}${this.lang.AUTO_HOME_RETURN}`);
-						return;
-					}
-					res.writeHead(SUCCESS_STATUS, { CONTENT_TYPE: CONTENT_TYPES.TEXT_HTML });
-					res.end(this.utils.readFileContent(fileName, data));
-				});
+				const fileName = readFileMatch[FIRST_CAPTURING_GROUP];
+				this.utils.handleReadFile(fileName, res, { SUCCESS_STATUS, NOT_FOUND_STATUS }, CONTENT_TYPES);
 				return;
 			}
 
@@ -91,37 +78,23 @@ class LabServer {
 				case ROUTES.GET_DATE:
 					const name = parsedUrl.query.name || DEFAULT_NAME;
 					const now = new Date();
-					const greetingMessage = this.utils.greeting(name, now);
-					let html = this.utils.getDate(greetingMessage);
-					const logMessage = `${greetingMessage.trim()}${NEW_LINE}`;
-
-					fs.appendFile(FILES.GREETING_FILE, logMessage + NEW_LINE, { encoding: UTF8 }, function(err) {
-						if (err) {
-							console.error(this.lang.MESSAGE.FILE_WRITE_ERROR, err);
-						} else {
-							console.log(this.lang.MESSAGE.FILE_WRITE_SUCCESS);
-						}
-					}.bind(this));
-
-					res.writeHead(SUCCESS_STATUS, { CONTENT_TYPE: CONTENT_TYPES.TEXT_HTML });
-					res.end(html);
+					this.utils.handleGreeting(name, now, res, { SUCCESS_STATUS }, CONTENT_TYPES, FILES.GREETING_FILE);
 					break;
 
 				case ROUTES.WRITE_FILE:
 					const textToAppend = parsedUrl.query.text;
 					if (textToAppend) {
 						const dataToAppend = textToAppend.trim() + NEW_LINE;
-						fs.appendFile(FILES.WRITING_FILE, dataToAppend, { encoding: UTF8 }, function(err) {
-							let pageMessage = '';
-							if (err) {
-								console.error(this.lang.MESSAGE.FILE_WRITE_ERROR, err);
-								pageMessage = `${this.lang.ERROR}${this.lang.MESSAGE.FILE_WRITE_ERROR} ${err.message}${this.lang.CLOSE_P}`;
-							} else {
-								pageMessage = `${this.lang.SUCCESS}${this.lang.MESSAGE.FILE_WRITE_SUCCESS}${this.lang.TEXT_ADDED}${textToAppend}${this.lang.TEXT_TO}${FILES.WRITING_FILE}${this.lang.CLOSE_P}`;
-							}
-							res.writeHead(SUCCESS_STATUS, { CONTENT_TYPE: CONTENT_TYPES.TEXT_HTML });
-							res.end(pageMessage + this.lang.AUTO_HOME_RETURN);
-						}.bind(this));
+						this.utils.handleWriteFile(
+							FILES.WRITING_FILE,
+							dataToAppend,
+							res,
+							{ SUCCESS_STATUS, NOT_FOUND_STATUS },
+							CONTENT_TYPES,
+							this.lang.MESSAGE.FILE_WRITE_SUCCESS,
+							this.lang.MESSAGE.FILE_WRITE_ERROR,
+							this.lang.AUTO_HOME_RETURN
+						);
 					} else {
 						res.writeHead(NOT_FOUND_STATUS, { CONTENT_TYPE: CONTENT_TYPES.TEXT_HTML });
 						res.end(`${this.lang.ERROR}${this.lang.WARNING_TEXTLESS}${this.lang.CLOSE_P}${this.lang.AUTO_HOME_RETURN}`);
@@ -136,6 +109,9 @@ class LabServer {
 		}
 	}
 
+	/**
+	 * Starts the HTTP server.
+	 */
 	start() {
 		const server = createServer(this.handleRequest.bind(this));
 		server.listen(PORT, () => {
